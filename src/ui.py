61c5cd6fd8817482aa8b06c7e8329669b1494d57
linganesan <linganesan.12@cse.mrt.ac.lk>
2017-03-30 19:41:25 -0400

    update ui.py

--- src/ui.py
+++ src/ui.py
@@ -14,8 +14,6 @@ import tempfile
 from core import diff as d
 from core import loader
 
-global factor
-
 def init():
     global menucanvas
     global diffcanvas
@@ -23,9 +21,11 @@ def init():
     global difflabel
     global slidebar
     global displaycanvas
-    global choice
-
-    choice =1
+    global factor
+    global btn_diff
+    global img2
+    global img1
+    img1 = img2 = None
 
     menucanvas = Canvas(frame)
     diffcanvas = Canvas(frame)
@@ -43,12 +43,17 @@ def init():
     btn_diff = Button(diffcanvas,text="Diff", width=10, command=diff)
     btn_diff.pack(side=BOTTOM,padx=10, pady=5)
 
-
     menucanvas.pack(side=TOP,anchor=N)
     diffcanvas.pack(side=TOP,anchor=N)
+    # create_btns()
 
 
 def create_btns():
+    global btn_2_up
+    global btn_swipe
+    global btn_onion
+    global btn_differ
+
     btn_2_up = Button( text="2 up", width=10, command=twoup)
     btn_2_up.pack(side=LEFT,padx=10,pady=5)
     btn_2_up.place(anchor=S, x=150, y=400)
@@ -57,27 +62,49 @@ def create_btns():
     btn_swipe.pack(side=LEFT,padx=10, pady=5)
     btn_swipe.place(anchor=S, x=250, y=400)
 
-
     btn_onion = Button(text="Onion skin", width=10, command=onion)
     btn_onion.pack(side=LEFT,padx=10, pady=5)
     btn_onion.place(anchor=S, x=350, y=400)
 
 
+    btn_differ = Button(text="Difference", width=10, command=differ)
+    btn_differ.pack(side=LEFT,padx=10, pady=5)
+    btn_differ.place(anchor=S, x=450, y=400)
+
+
+
 def twoup():
     slidebar.place_forget()
     difflabel.place_forget()
-    label1.place(x=50, y=100)
-    label2.place(x=260, y=100)
-    pass
+    label1.place(x=100, y=100)
+    label2.place(x=310, y=100)
+    # btn_2_up.config(state='disabled')
+
+def differ():
+    slidebar.place_forget()
+    difflabel.place_forget()
+    showdiff(diffimage())
+    # btn_differ.config(state='disabled')
+
 
 def swipe():
-    slidebar.place(anchor=S, x=250, y=360)
+    global choice
+    label1.place_forget()
+    label2.place_forget()
+    difflabel.place(x=200, y=100)
+    choice =1
+    slidebar.place(anchor=S, x=300, y=360)
+    # btn_swipe.config(state='disabled')
 
-    pass
 
 def onion():
-    slidebar.place(anchor=S, x=250, y=360)
-    pass
+    global choice
+    label1.place_forget()
+    label2.place_forget()
+    difflabel.place(x=200, y=100)
+    choice =2
+    slidebar.place(anchor=S, x=300, y=360)
+    # btn_onion.config(state='disabled')
 
 def diffimage():
     diffimg = d.diff(openedimage1,openedimage2)
@@ -90,7 +117,7 @@ def showdiff(tempimg):
     global viewWindow
     temp_dir = tempfile.mkdtemp()
     filename, extension = splitext(openedimage1)
-    temp_file = temp_dir+'/dddd'+extension
+    temp_file = temp_dir+'/diff'+extension
     loader.save_image(tempimg,temp_file)
     img = tk_image(temp_file,200,200)
     im3 = ImageTk.PhotoImage(img)
@@ -100,21 +127,25 @@ def showdiff(tempimg):
 
 
     difflabel= Label(image=im3)
-    difflabel.place(x=150, y=100)
+    difflabel.place(x=200, y=100)
 
     displaycanvas.image = im3
     displaycanvas.create_image(0, 0, anchor=CENTER, image=im3, tags="bg_img")
 
 
 def diff():
-    label1.place_forget()
-    label2.place_forget()
-    tempimg = diffimage()
-    showdiff(tempimg)
-    create_btns()
+    if img1!=None and img2!=None:
+        label1.place_forget()
+        label2.place_forget()
+        tempimg = diffimage()
+        showdiff(tempimg)
+        create_btns()
+        btn_diff.config(state='disabled')
+    else:
+        showwarning("Load Error","You haven't selected both image")
 
 def sidebar():
-    scale = Scale(slidebar,orient='horizontal', from_=0, to=200, showvalue=0,command=sidebarScale, length=200)
+    scale = Scale(slidebar,orient='horizontal', from_=0, to=100, showvalue=0,command=sidebarScale, length=200)
     scale.pack(side = "top")
 
 def tk_image(path,w,h):
@@ -139,11 +170,12 @@ def LoadImage1():
     openedimage1 = file_open()
     print openedimage1
     img1 = tk_image(openedimage1,200,200)
-    im1 = ImageTk.PhotoImage(img1)
+    print img1
 
+    im1 = ImageTk.PhotoImage(img1)
     print im1
     label1 = Label(image=im1)
-    label1.place(x=50, y=100)
+    label1.place(x=100, y=100)
 
 
 def LoadImage2():
@@ -157,45 +189,40 @@ def LoadImage2():
     openedimage2 = file_open()
     print openedimage2
     img2 = tk_image(openedimage2,200,200)
+    print img2
     im2 = ImageTk.PhotoImage(img2)
 
     print im2
     label2= Label(image=im2)
-    label2.place(x=260, y=100)
+    label2.place(x=310, y=100)
 
 def sidebarScale(val):
-    if(choice == 1):
-        imD= slideImage(img1,img2,int(val))
-        tempimg = ImageTk.PhotoImage(imD)
+    if choice == 1:
+        tempimg= slideImage(img1,img2,int(val))
+        tempimg = ImageTk.PhotoImage(tempimg)
     else:
-        imD= overlayImage(img1,img2,int(val))
-        tempimg = ImageTk.PhotoImage(imD)
+        tempimg= onionSkinImage(img1,img2,int(val))
+        tempimg = ImageTk.PhotoImage(tempimg)
+
     difflabel.config(image=tempimg)
-    difflabel.place(x=150, y=100)
+    difflabel.place(x=200, y=100)
     displaycanvas.image = tempimg
     displaycanvas.create_image(0, 0, anchor=CENTER, image=tempimg, tags="bg_img")
 
 
-def diffTransImage(img1,img2):
-	imNew = Image.new('RGBA',(img1.size),'white')
-	for i in range(img1.size[0]):
-		for j in range(img1.size[1]):
-			imNew.putpixel((i,j),diffpix(img1.getpixel((i,j)),img2.getpixel((i,j))))
-	return imNew
 
-
-def overlayImage(img1,img2,alpha):
+def onionSkinImage(img1,img2,value):
 	try:
-		return Image.blend(img1, img2, float(alpha)/100)
+		return Image.blend(img1, img2, float(value)/100)
 	except ValueError:
-		return Image.blend(img1.convert('RGB'), img2.convert('RGB'), float(alpha)/100)
+		return Image.blend(img1.convert('RGB'), img2.convert('RGB'), float(value)/100)
 
 
-def slideImage(img1,img2,percent):
+def slideImage(img1,img2,value):
 	imNew = Image.new('RGB',(img1.size),'white')
 	sizex=img1.size[0]
 	sizey=img1.size[1]
-	x = (sizex*percent)/100
+	x = (sizex*value)/100
 	img1crop = img1.crop((x,0,sizex,sizey))
 	img2crop = img2.crop((0,0,x,sizey))
 	imNew.paste(img1crop,(x,0))
@@ -205,7 +232,7 @@ def slideImage(img1,img2,percent):
 root =Tk()
 
 root.title("Image-Diff Tool")
-root.minsize(500, 400)
+root.minsize(600, 410)
 frame = Frame(root)
 frame.pack(expand=NO,fill=BOTH)
 
